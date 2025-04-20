d4cb0ea043d3f16c60f45f930ea4591340cd2e1b
rachit976 <mukeshsoni38@gmail.com>
2025-04-20 20:01:38 +0530

    Add files via upload

---
+++ 234.py
@@ -0,0 +1,13 @@
+test_dict ={'codingal': 2, 'is', 2 'best', 2 'for', 2 ,'coding'   : 1 
+               
+            }
+print("the original dictionary :"+ str  (test_dict))
+k = 2
+res = 0
+for keyin test_dict:
+if test_dict[key] == k:
+    res =res +1
+
+    print("frequeny of k is :"+ str(res))
+    
+
